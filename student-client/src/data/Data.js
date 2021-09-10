b11f802720d56a8a3b285f1b4b87395d2030530f
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-09-10 14:29:10 +0200

    refacroting data

---
+++ student-client/src/data/Data.js
@@ -0,0 +1,123 @@
+import { Link } from "react-router-dom";
+
+
+
+
+
+export const rows = [
+  {
+    id: 1,
+    company: "Volvo",
+    email: "ribet@optonline.net",
+    utbildningar: ["java", "linux", "php"],
+    hemsida: "volvo.se",
+    favoriter: true,
+  },
+  {
+    id: 2,
+    company: "Google",
+    email: "johndo@live.com",
+    hemsida: "google.se",
+    utbildningar: ["react", "typescript", "php"],
+  },
+  {
+    id: 3,
+    company: "NetonNet",
+    email: "andersbr@yahoo.com",
+    utbildningar: ["flutter", "typescript", "firebase"],
+    hemsida: "NetonNet.se",
+    favoriter: true,
+  },
+  {
+    id: 4,
+    company: "MediaMarkt",
+    email: "reziac@me.com",
+    utbildningar: ["Kotlin", "tdd"],
+    hemsida: "MediaMarkt.se",
+    favoriter: true,
+  },
+  {
+    id: 5,
+    company: "elgiganten",
+    email: "moonlapse@aol.com",
+    utbildningar: ["Redis", "Graphql"],
+    hemsida: "elgiganten.se",
+    favoriter: false,
+  },
+  {
+    id: 6,
+    company: "Amazon",
+    email: "mbswan@sbcglobal.net",
+    utbildningar: ["Junit 5", "MongoDb"],
+    hemsida: "Amazon.se",
+
+    favoriter: false,
+  },
+  {
+    id: 7,
+    company: "Netflix",
+    email: "floxy@icloud.com",
+    hemsida: "Netflix.se",
+    favoriter: false,
+  },
+  {
+    id: 8,
+    company: "Facebook",
+    email: "Facebook@icloud.com",
+    hemsida: "Facebook.se",
+    favoriter: false,
+  },
+  {
+    id: 9,
+    company: "Beats",
+    email: "Beats@icloud.com",
+    hemsida: "Beats.se",
+    favoriter: false,
+  },
+  {
+    id: 10,
+    company: "Netflix",
+    email: "floxy@icloud.com",
+    hemsida: "volvo.se",
+    favoriter: false,
+  },
+  {
+    id: 11,
+    company: "Netflix",
+    email: "floxy@icloud.com",
+    hemsida: "volvo.se",
+    favoriter: false,
+  },
+  {
+    id: 12,
+    company: "Netflix",
+    email: "floxy@icloud.com",
+    hemsida: "volvo.se",
+    favoriter: false,
+  },
+  {
+    id: 13,
+    company: "Netflix",
+    email: "floxy@icloud.com",
+    hemsida: "volvo.se",
+    favoriter: false,
+  },
+  {
+    id: 14,
+    company: "Netflix",
+    email: "floxy@icloud.com",
+    hemsida: "volvo.se",
+    favoriter: false,
+  },
+  {
+    id: 15,
+    company: "Netflix",
+    email: "floxy@icloud.com",
+    hemsida: "volvo.se",
+    favoriter: false,
+  },
+];
+
+export const columns =[
+
+]
