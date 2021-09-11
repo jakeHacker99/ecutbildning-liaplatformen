3fafef3cb40de56d92ea4f6ad959f88d5965c685
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-09-11 02:26:36 +0200

    impl snackbar

--- student-client/src/screens/Table.tsx
+++ student-client/src/screens/Table.tsx
@@ -1,46 +1,85 @@
-import React from 'react'
-import {rows, columns} from "../data/Data"
-import { Teknologier } from '../data/Data2';
-
-import {
-    DataGrid
-} from "@material-ui/data-grid";
-import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
-import StarOutlinedIcon from '@material-ui/icons/StarOutlined';
+import React, { useState } from "react";
+import { rows, columns, Teknologier } from "../data/Data";
+import "../App.css";
 
 
+
+import { DataGrid } from "@material-ui/data-grid";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import StarOutlinedIcon from "@material-ui/icons/StarOutlined";
+import { Button } from "@material-ui/core";
+import { useSnackbar } from "notistack";
 
 const Table = () => {
-   const handleClick = () => {
-    
-  }
-  
-    return (
-
-    <div style={{display:"block", height: 400, minWidth: "450px", width:"960px", marginLeft:"20%", marginRight:"50%"  }}>
 
 
+  const { enqueueSnackbar } = useSnackbar();
+  const inputRef = React.useRef(null)
 
+    const handleClickVariant = (variant: any) => () => {
+    // variant could be success, error, warning, info, or default
+    enqueueSnackbar('Copied to clipboard', { variant });
+  };
+
+
+  const [mail, setMail] = useState("");
+  const [websiteLink, setWebsiteLink] = useState("Jakobs.se");
+
+  return (
+    <div
+      style={{
+        display: "block",
+        height: 400,
+        minWidth: "450px",
+        width: "960px",
+        marginLeft: "20%",
+        marginRight: "50%",
+        paddingTop: "100px",
+      }}
+    >
       <DataGrid
-        columns={ [
-          { field: "company",   width: 150 },
-          { field: "email",    width: 180 },
-          { field: "utbildningar",     width: 320 },
-          { field: "hemsida",     width: 320, renderCell: (cellValue) =>       {
-            return <a  href={cellValue.row.hemsida} target="_blank" rel="noreferrer"
-            >{cellValue.row.hemsida}</a>
-          }},
-          {field: "favoriter", width:140, renderCell:(cellValue) => {
+        columns={[
+          { field: "Företag", width: 150 },
+          { field: "Kontakt", width: 180 },
+          { field: "Utbildningar", width: 320 },
+          {
+            field: "Hemsida",
+            width: 320,
+            renderCell: (cellValue) => {
+              return (
+                <>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => {
+                    setWebsiteLink(cellValue.row.Hemsida);
+                    window.navigator.clipboard.writeText(websiteLink);
+                    // @ts-ignore
+                    inputRef.current.click();
+                  }}
+                >
 
+
+                  {cellValue.row.Hemsida}
+                </Button>
+                <a ref={inputRef} className="custom-link" onClick={handleClickVariant('info')}> </a>
+
+                </>
+              );
+            },
+          },
+          {
+            field: "favoriter",
+            width: 140,
+            renderCell: (cellValue) => {
               if (cellValue.row.favoriter) {
                 return <StarBorderOutlinedIcon  >a</StarBorderOutlinedIcon>;
               } else {
                 return <StarOutlinedIcon>b</StarOutlinedIcon>;
               }
-          }}
-
+            },
+          },
         ]}
-        
         rows={rows}
         pageSize={15}
         checkboxSelection
@@ -50,5 +89,4 @@ const Table = () => {
   );
 };
 
-
-export default Table
+export default Table;
